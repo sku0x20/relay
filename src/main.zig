0f16453622f18acffe3c16aed006f4f188cebe37
siddhant <siddhantkumarupmanyu@gmail.com>
2026-01-23 18:44:27 +0530

    Remove unused `printStdout` function and redundant test

--- src/main.zig
+++ src/main.zig
@@ -2,13 +2,5 @@ const std = @import("std");
 const relay = @import("relay");
 
 pub fn main() !void {
-    try relay.printStdout();
 }
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
