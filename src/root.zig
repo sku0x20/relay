6444c6da103d505c101f8dea522e2c5142ccb2bc
siddhant <siddhantkumarupmanyu@gmail.com>
2026-01-23 18:27:14 +0530

    Refactor stdout handling and integrate `printStdout` function for streamlined output management

--- src/root.zig
+++ src/root.zig
@@ -7,17 +7,20 @@ pub fn bufferedPrint() !void {
     // stdout, not any debugging messages.
     var stdout_buffer: [1024]u8 = undefined;
     var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
-    const stdout = &stdout_writer.interface;
+    const stdout1 = &stdout_writer.interface;
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    try stdout1.print("Run `zig build test` to run the tests.\n", .{});
 
-    try stdout.flush(); // Don't forget to flush!
+    try stdout1.flush(); // Don't forget to flush!
 }
 
-pub fn add(a: i32, b: i32) i32 {
-    return a + b;
+const stdout = initWriter();
+fn initWriter() *std.Io.Writer {
+    var stdout_buffer: [1024]u8 = undefined;
+    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
+    return &stdout_writer.interface;
 }
 
-test "basic add functionality" {
-    try std.testing.expect(add(3, 7) == 10);
+pub fn printStdout() void {
+    stdout.print();
 }
