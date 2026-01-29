4c5588923813653263d2de37e9153b208a375760
siddhant <siddhantkumarupmanyu@gmail.com>
2026-01-29 18:41:11 +0530

    Run single e2e runner process

--- e2e/e2e.test.zig
+++ e2e/e2e.test.zig
@@ -1,5 +1,17 @@
 const std = @import("std");
+test "e2e" {
+    const exe_path = try std.process.getEnvVarOwned(std.testing.allocator, "RELAY_BIN");
+    defer std.testing.allocator.free(exe_path);
 
-test "relaySingleTcpClient" {
-    try std.testing.expect(1 == 1);
+    var child = std.process.Child.init(&.{ exe_path }, std.testing.allocator);
+    child.stdin_behavior = .Ignore;
+    child.stdout_behavior = .Inherit;
+    child.stderr_behavior = .Inherit;
+    try child.spawn();
+    defer {
+        _ = try child.kill();
+        _ = try child.wait();
+    }
+
+    std.time.sleep(100 * std.time.ns_per_ms);
 }
