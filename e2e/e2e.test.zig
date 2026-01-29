0e14b3d396199f0923977ea630b7ff551aab0c8e
siddhant <siddhantkumarupmanyu@gmail.com>
2026-01-29 18:46:14 +0530

    Handle e2e cleanup errors in defer

--- e2e/e2e.test.zig
+++ e2e/e2e.test.zig
@@ -1,16 +1,21 @@
 const std = @import("std");
+
 test "e2e" {
     const exe_path = try std.process.getEnvVarOwned(std.testing.allocator, "RELAY_BIN");
     defer std.testing.allocator.free(exe_path);
 
-    var child = std.process.Child.init(&.{ exe_path }, std.testing.allocator);
+    var child = std.process.Child.init(&.{exe_path}, std.testing.allocator);
     child.stdin_behavior = .Ignore;
     child.stdout_behavior = .Inherit;
     child.stderr_behavior = .Inherit;
     try child.spawn();
     defer {
-        _ = try child.kill();
-        _ = try child.wait();
+        _ = child.kill() catch |err| {
+            std.debug.print("e2e: kill failed: {s}\n", .{@errorName(err)});
+        };
+        _ = child.wait() catch |err| {
+            std.debug.print("e2e: wait failed: {s}\n", .{@errorName(err)});
+        };
     }
 
     std.time.sleep(100 * std.time.ns_per_ms);
