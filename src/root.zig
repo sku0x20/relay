4580f65e774d5097810198adee5919795865d584
siddhant <siddhantkumarupmanyu@gmail.com>
2026-01-30 13:39:41 +0530

    Refactor connection handling by extracting logic into `handleConnection`

--- src/root.zig
+++ src/root.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const Server = std.net.Server;
 
 pub fn startRelay() !void {
     const address = try std.net.Address.parseIp("127.0.0.1", 19000);
@@ -9,17 +10,21 @@ pub fn startRelay() !void {
 
     while (true) {
         const connection = try server.accept();
-        defer connection.stream.close();
-
-        var reader = connection.stream.reader(&.{});
-        var buf: [4]u8 = undefined;
-        reader.interface().readSliceAll(&buf) catch |err| switch (err) {
-            error.EndOfStream => continue,
-            else => return err,
-        };
-        if (!std.mem.eql(u8, &buf, "ping")) continue;
-
-        var stream_writer = connection.stream.writer(&.{});
-        try stream_writer.interface.writeAll("pong");
+        try handleConnection(&connection);
     }
+}
+
+pub fn handleConnection(connection: *const Server.Connection) !void {
+    defer connection.stream.close();
+
+    var reader = connection.stream.reader(&.{});
+    var buf: [4]u8 = undefined;
+    reader.interface().readSliceAll(&buf) catch |err| switch (err) {
+        error.EndOfStream => return,
+        else => return err,
+    };
+    if (!std.mem.eql(u8, &buf, "ping")) return;
+
+    var stream_writer = connection.stream.writer(&.{});
+    try stream_writer.interface.writeAll("pong");
 }
