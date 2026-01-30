eece6f96650a1ef1ff5c3b9a39199ed51de91bf5
siddhant <siddhantkumarupmanyu@gmail.com>
2026-01-30 13:12:50 +0530

    Test multiple e2e clients

--- e2e/e2e.test.zig
+++ e2e/e2e.test.zig
@@ -23,11 +23,22 @@ fn pingMultiple() !void {
     var client1 = try std.net.tcpConnectToHost(std.testing.allocator, "127.0.0.1", 19000);
     defer client1.close();
 
+    var client2 = try std.net.tcpConnectToHost(std.testing.allocator, "127.0.0.1", 19000);
+    defer client2.close();
+
     var c1_writer = client1.writer(&.{});
     try c1_writer.interface.writeAll("ping");
+
+    var c2_writer = client2.writer(&.{});
+    try c2_writer.interface.writeAll("ping");
 
     var c1_reader = client1.reader(&.{});
     var c1_buf: [4]u8 = undefined;
     try c1_reader.interface().readSliceAll(&c1_buf);
     try std.testing.expect(std.mem.eql(u8, &c1_buf, "pong"));
+
+    var c2_reader = client2.reader(&.{});
+    var c2_buf: [4]u8 = undefined;
+    try c2_reader.interface().readSliceAll(&c2_buf);
+    try std.testing.expect(std.mem.eql(u8, &c2_buf, "pong"));
 }
