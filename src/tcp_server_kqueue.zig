8b4e3a79b9dc48e8afe9f754c8ac31609ae1d9f1
siddhant <siddhantkumarupmanyu@gmail.com>
2026-02-09 06:47:20 +0530

    Add `listen` call to `tcpServerKqueue` and define `listen` helper function

--- src/tcp_server_kqueue.zig
+++ src/tcp_server_kqueue.zig
@@ -8,6 +8,8 @@ pub fn start(
     const socket = try createSocket();
     const addr = try std.net.Ip4Address.resolveIp(bind, port);
     try bindSocket(socket, addr);
+    try listen(socket);
+    // try posix.accept(socket, , addr_size: ?*u32, flags: u32)
     posix.nanosleep(10000, 0);
 }
 
@@ -56,4 +58,8 @@ fn bindSocket(socket_fd: posix.socket_t, addr: std.net.Ip4Address) !void {
         @ptrCast(&(addr.sa)),
         addr.getOsSockLen(),
     );
+}
+
+fn listen(socket_fd: posix.socket_t) !void{
+    try posix.listen(socket_fd, 10);
 }
