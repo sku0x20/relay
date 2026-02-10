fb536a82de5f51f3b60322d13675ad69d749f256
siddhant <siddhantkumarupmanyu@gmail.com>
2026-02-10 12:18:32 +0530

    Remove `tcp_server.zig` and related usage in favor of `tcp_server_posix`.

--- src/root.zig
+++ src/root.zig
@@ -1,28 +1,9 @@
 const std = @import("std");
-const Thread = std.Thread;
-const tcpServer = @import("tcp_server.zig");
 const tcpServerPosix = @import("tcp_server_posix.zig");
 
 pub fn startRelay() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit(); // todo: check for leaks!
-
-    const allocator = gpa.allocator();
-
-    const concurrency = 1024;
-
-    var pool: Thread.Pool = undefined;
-    try pool.init(.{
-        .allocator = allocator,
-        .n_jobs = concurrency,
-    });
-    defer pool.deinit();
-
     const bind = "127.0.0.1";
     const port = 19000;
-
-    // const max_connections = concurrency;
-    // try tcpServer.start(bind, port, max_connections, &pool);
 
     try tcpServerPosix.start(bind, port);
 }
