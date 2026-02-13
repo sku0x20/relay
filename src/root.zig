58f8b2161e95631b7851eb7b25a734e22f2a5f97
siddhant <siddhantkumarupmanyu@gmail.com>
2026-02-14 01:28:23 +0530

    Add platform-specific handling for `startRelay` in `root.zig`; warn on unsupported OS.

--- src/root.zig
+++ src/root.zig
@@ -1,9 +1,19 @@
 const std = @import("std");
+const buildin = @import("builtin");
+// todo: do something like this
+// const Impl = switch (builtin.os.tag) {
+//    .linux => @import("impl_linux.zig"),
+//    .windows => @import("impl_windows.zig"),
+//    else => @compileError("Unsupported OS"),
+//};
 const tcpServerKq = @import("tcp_server_kq.zig");
 
 pub fn startRelay() !void {
     const bind = "127.0.0.1";
     const port = 19000;
 
-    try tcpServerKq.start(bind, port);
+    switch (buildin.os.tag) {
+        .freebsd, .macos => try tcpServerKq.start(bind, port),
+        else => std.log.warn("platform not available", .{}),
+    }
 }
