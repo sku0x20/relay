c62139f2856844397620afaced20dc6e79a847cd
siddhant <siddhantkumarupmanyu@gmail.com>
2026-02-15 17:02:42 +0530

    Add test steps outline to `TcpServer.test.zig`

--- test/posix/TcpServer.test.zig
+++ test/posix/TcpServer.test.zig
@@ -4,3 +4,15 @@ const TcpServer = @import("src/posix/TcpServer.zig");
 test "passing" {
     try std.testing.expect(1 == 1);
 }
+
+// create the server
+// startListening
+// get port
+// create client
+// client init connection
+// write from client
+// read from the server; assert it
+// write to the server;
+// read from client; assert it
+// close the client
+// stop the server
